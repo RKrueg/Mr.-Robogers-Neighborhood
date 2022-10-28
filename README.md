4cac0a78abc2daee51d35978aed9274a82b4c0af
RKrueg <thegckid@gmail.com>
2022-10-28 13:24:04 -0700

    change the number to 'Beep!' if that number contains a 1

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Test: "It should return an array of numbers from 0 to the user's inputted number
 Code: numInput(7);
 Expected Output: [0,1,2,3,4,5,6,7]
 
+Describe: containesA1
+
 Test: "Check to see if a number contains a 1."
 Code: numInput(10)
 Expected Output: True
+
+Test: "If a number contains a 1, all digits are replaced with "Beep!"
+Code: numInput(10)
+Expected Output: [0, "Beep!",3,4,5,6,7,8,9,"Beep!"]
--- js/styles.js
+++ js/styles.js
@@ -9,13 +9,19 @@ function numInput (inputNum) {
 function containesA1 (inputNum) {
   let hasA1 = false;
   let beep = inputNum + "".split("");
-  if (beep[0] === '1' || beep[1] === '1' || beep[3] === '1' || beep[4] === '1') {
+  if (beep[0] === '1' || beep[1] === '1' || beep[2] === '1' || beep[3] === '1' || beep[4] === '1' || beep[5] === '1') {
     hasA1 = true;
   }
   return hasA1;
 }
 
+function rogers(inputNum) {
+  if (containesA1(inputNum) === true) {
+    return "Beep!"
+  } else {
 
+  }
+}
 
 
 
