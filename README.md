c99eb0a9315cd3845ea1ccdc524311158d5dc2b1
RKrueg <thegckid@gmail.com>
2022-10-28 13:06:06 -0700

    check to see if a number contains 1 and return true if it does or false if it does not

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-```Describe: collectInput()
+```Describe: numInput()
 
 Test: "It should return an array of numbers from 0 to the user's inputted number"
-Code: collectInput(7);
+Code: numInput(7);
 Expected Output: [0,1,2,3,4,5,6,7]
+
+Test: "Check to see if a number contains a 1."
+Code: numInput(10)
+Expected Output: True
--- js/styles.js
+++ js/styles.js
@@ -6,4 +6,20 @@ function numInput (inputNum) {
   return newNum;
 }
 
-console.log(numInput(5))
+function containesA1 (inputNum) {
+  let hasA1 = false;
+  let beep = inputNum + "".split("");
+  if (beep[0] === '1' || beep[1] === '1' || beep[3] === '1' || beep[4] === '1') {
+    hasA1 = true;
+  }
+  return hasA1;
+}
+
+
+
+
+
+
+
+
+//console.log(numInput(5))
